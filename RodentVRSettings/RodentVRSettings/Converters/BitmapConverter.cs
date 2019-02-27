cc8ce808517d51cb19071627258a28cc0bb4c68a
westryank1 <west.ryan.k@gmail.com>
2019-02-27 11:22:47 -0700

    Commit changes I forgot to add
    I had uncommitted changes from a while back and
    I don't remember what they were, so I will just
    commit it before I make other changes.
    
    
    Former-commit-id: ea948583f3d14d9a422fde744da507b102cac208

---
+++ RodentVRSettings/RodentVRSettings/Converters/BitmapConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace RodentVRSettings.Converters
+{
+	public class BitmapConverter : MarkupExtension, IValueConverter
+	{
+		#region IValueConverter Implementation
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			System.Drawing.Bitmap bmp = (System.Drawing.Bitmap)value;
+			return bmp?.ToWpfBitmap();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			System.Windows.Media.Imaging.BitmapImage image = (System.Windows.Media.Imaging.BitmapImage)value;
+			return image?.ToGDIBitmap();
+		}
+		#endregion
+
+		#region MarkupExtension Implementation
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return this;
+		} 
+		#endregion
+	}
+}
---
+++ RodentVRSettings/RodentVRSettings/Views/RodentVRSettingsWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RodentVRSettings
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : MetroWindow//, Contracts.RodentVRSettingsViewContract
+	{
+		//public Contracts.RodentVRSettingsPresenterContract Presenter { get; set; }
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			//this.Presenter = new Presenters.BehaviorVisualizerPresenter();
+			//this.Presenter.View = this;
+			//this.Presenter.Reset();
+		}
+	}
+}
