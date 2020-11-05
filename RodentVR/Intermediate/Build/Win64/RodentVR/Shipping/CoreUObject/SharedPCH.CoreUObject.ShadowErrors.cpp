fb97aa407bed874fce64ff852e70d41e11f803ad
RyanKWest <west.ryan.k@gmail.com>
2020-11-05 01:10:27 -0700

    Remove Unintentionally Committed Files
    Back when I first created the project, I didn't have a .gitignore for
    UE4 (or atleast did it wrong...) So I have 9575 generated files
    (actual number) that I didn't want in the repo. I just found out that I
    can remove them so as to respect the gitignore now. This will clean up
    the repo quite a bit! Sorry to anyone who inherits this project, I'm
    still learning the ins and outs of UE4, and messed up with UE4 and git!

--- RodentVR/Intermediate/Build/Win64/RodentVR/Shipping/CoreUObject/SharedPCH.CoreUObject.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Ryan/Drive/BYU/2018_Fall/cs_497r/Rodent-VR/RodentVR/Intermediate/Build/Win64/RodentVR/Shipping/CoreUObject/SharedPCH.CoreUObject.ShadowErrors.h"
--- RodentVR/Intermediate/Build/Win64/RodentVR/Shipping/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Ryan/Drive/BYU/2018_Fall/cs_497r/Rodent-VR/RodentVR/Intermediate/Build/Win64/RodentVR/Shipping/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.h"
